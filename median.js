8e618cf7b088153863474d033ad5c9281815ba70
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-03-21 18:16:53 +0100

    Final commit

--- median.js
+++ median.js
@@ -1,10 +1,38 @@
+// // const Median = () => {
+//     let arr = [1, 10, 6, 4, 8, 3, 7, 5, 9, 2, 11, 12, 13];
+//     let n = arr.length;
+
+//     const sortedarr = arr.sort((num1, num2) => num1 - num2);
+//     const reqnum = n / 2;
+//     if((n % 2) == 1) {
+//       var round =  Math.round(reqnum)
+//         console.log(sortedarr[round])
+//     }else{
+//         console.log("even")
+//     }
+//     const req = sortedarr[reqnum];
+//     console.log(req)
+//     // return req
+// // }
+
 const Median = () => {
-    let arr = [1, 10, 6, 4, 8, 3, 7, 5, 9, 2, 5];
+    let arr = [1, 10, 6, 4, 8, 3, 7, 5, 9, 2, 11, 12, 13];
     let n = arr.length;
 
     const sortedarr = arr.sort((num1, num2) => num1 - num2);
-    const req = sortedarr[9];
-    const reqnum = n % 2;
-    return reqnum
+    const reqnum = n / 2;
+    
+    if (n % 2 === 1) {
+        // If the array length is odd
+        var medianIndex = Math.floor(reqnum);
+        console.log("Median:", sortedarr[medianIndex]);
+    } else {
+        // If the array length is even
+        var medianIndex1 = reqnum - 1;
+        var medianIndex2 = reqnum;
+        var median = (sortedarr[medianIndex1] + sortedarr[medianIndex2]) / 2;
+        console.log("Median:", median);
+    }
 }
-console.log(Median())
+
+Median();
